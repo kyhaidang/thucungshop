6ab7b252cd43fd6b48c839c7bfc32dbbd890216a
Ký Hải Đăng <kyhaidang123@gmail.com>
2024-12-04 21:02:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import ProductList from './pages/ProductList';
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
 import Contact from './pages/Contact';
-import Account from './pages/Account';  // Đường dẫn tới trang Account
-import DogProducts from './pages/DogProducts';  // Thêm import
-import SpaServices from './pages/SpaServices';  // Thêm import
+import Account from './pages/Account';
+import DogProducts from './pages/DogProducts';
+import SpaServices from './pages/SpaServices';
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/Homepage" element={<HomePage />} />
         <Route path="/products" element={<ProductList />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/account" element={<Account />} />  {/* Route tới trang tài khoản */}
-        <Route path="/dog-products" element={<DogProducts />} />      {/* Mua đồ cho chó */}
-        <Route path="/spa-services" element={<SpaServices />} />      {/* Dịch vụ spa */}
+        <Route path="/account" element={<Account />} />
+        <Route path="/dog-products" element={<DogProducts />} />
+        <Route path="/spa-services" element={<SpaServices />} />
       </Routes>
     </Router>
   );
